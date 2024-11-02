b6a53460d1067a3015e5b0ea2f0c41d8210a563d
DarthPackman <packman150@hotmail.com>
2024-11-01 23:52:14 -0700

    update

--- Distance/Distance/prog25.cpp
+++ Distance/Distance/prog25.cpp
@@ -1,4 +1,3 @@
-// @: Complete the function definition in distance.cpp and run this file
 #include "Distance.h"
 #include <iostream>
 #include <conio.h>
@@ -9,40 +8,53 @@ Distance operator + (Distance &ob1, Distance &ob2)
     Distance temp;
     temp.feet   = ob1.feet   + ob2.feet;
     temp.inches = ob1.inches + ob2.inches;
-    if(temp.inches > 12)
+    if(temp.inches >= 12)
     {
         temp.inches -= 12;
         temp.feet++;
     }
-    return(temp);
+    return temp;
 }
 
 Distance operator - (Distance &ob1, Distance &ob2)
 {
-    //@: Complete the code for the difference operation
-
+    Distance temp;
+    temp.feet   = ob1.feet   - ob2.feet;
+    temp.inches = ob1.inches - ob2.inches;
+    if(temp.inches < 0)
+    {
+        temp.inches += 12;
+        temp.feet--;
+    }
+    return temp;
 }
 
-Distance operator *(int d, Distance &ob)
+Distance operator * (int d, Distance &ob)
 {
-    //@: Complete the code for the multiplication
+    Distance temp;
+    int totalInches = (ob.feet * 12 + ob.inches) * d;
+    temp.feet = totalInches / 12;
+    temp.inches = totalInches % 12;
+    return temp;
 }
 
-int operator ==(Distance &ob1, Distance &ob2)
+int operator == (Distance &ob1, Distance &ob2)
 {
-    //@: complete the code
+    return (ob1.feet == ob2.feet && ob1.inches == ob2.inches);
 }
 
 int operator < (Distance &ob1, Distance &ob2)
 {
-    //@: complete the code
-
+    int totalInches1 = ob1.feet * 12 + ob1.inches;
+    int totalInches2 = ob2.feet * 12 + ob2.inches;
+    return (totalInches1 < totalInches2);
 }
 
 int operator > (Distance &ob1, Distance &ob2)
 {
-    //@: complete the code
-
+    int totalInches1 = ob1.feet * 12 + ob1.inches;
+    int totalInches2 = ob2.feet * 12 + ob2.inches;
+    return (totalInches1 > totalInches2);
 }
 
 int main()
@@ -51,105 +63,90 @@ int main()
     Distance ob2;
     Distance ob3;
 
-    cout<<"\n=====Enter Data for OBJECT1=====\n";
+    cout << "\n=====Enter Data for OBJECT1=====\n";
     ob1.getdata();
-    cout<<"\n\n=====Enter Data for OBJECT2=====\n";
+    cout << "\n\n=====Enter Data for OBJECT2=====\n";
     ob2.getdata();
-    int opt=0;
-    int choice=1,data;
-    while(choice !=0)
-    {
-        opt=0;
 
-        cout<<"\nChose your choice\n";
-        cout<<"1)  Addition            ( + )\n";
-        cout<<"2)  Subtraction         ( - )\n";
-        cout<<"3)  Multiplication      ( * )\n";
-        cout<<"4)  Comparision         ( == )\n";
-        cout<<"5)  Comparision         ( < )\n";
-        cout<<"6)  Comparision         ( > )\n";
-        cout <<"0)  Quit                \n";
-        cout<<"Enter your choice:-";
-        cin>>choice;
-        cout<<endl<<endl;
+    int opt = 0;
+    int choice = 1, data;
+    while(choice != 0)
+    {
+        opt = 0;
+
+        cout << "\nChoose your choice\n";
+        cout << "1)  Addition            ( + )\n";
+        cout << "2)  Subtraction         ( - )\n";
+        cout << "3)  Multiplication      ( * )\n";
+        cout << "4)  Comparison          ( == )\n";
+        cout << "5)  Comparison          ( < )\n";
+        cout << "6)  Comparison          ( > )\n";
+        cout << "0)  Quit                \n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cout << endl << endl;
         switch(choice)
         {
-        case 1 :
-        {
-            ob3 = ob1 + ob2;
-            opt=1;
-            break;
+            case 1 :
+                ob3 = ob1 + ob2;
+                opt = 1;
+                break;
+            case 2 :
+                ob3 = ob1 - ob2;
+                opt = 1;
+                break;
+            case 3 :
+                cout << "\nEnter integer to be multiplied: ";
+                cin >> data;
+                ob3 = data * ob1;
+                opt = 1;
+                break;
+            case 4 :
+                if(ob1 == ob2)
+                {
+                    cout << "\nBoth Objects are equal.\n";
+                }
+                else
+                {
+                    cout << "\nThey are Unequal.\n";
+                }
+                getch();
+                break;
+            case 5 :
+                if(ob1 < ob2)
+                {
+                    cout << "\nObject1 is less than Object2\n";
+                }
+                else
+                {
+                    cout << "\nObject2 is less than Object1\n";
+                }
+                getch();
+                break;
+            case 6 :
+                if(ob1 > ob2)
+                {
+                    cout << "\nObject1 is greater than Object2\n";
+                }
+                else
+                {
+                    cout << "\nObject2 is greater than Object1\n";
+                }
+                getch();
+                break;
+            case 0:
+                cout << "\n\nHave a nice day...\n";
+                getch();
+                break;
+            default :
+                cout << "Invalid option ";
         }
-        case 2 :
+        if (opt == 1)
         {
-            ob3 = ob1 - ob2;
-            opt=1;
-            break;
-        }
-        case 3 :
-            cout<<"\nEnter integer to be multiplied:-";
-            cin>>data;
-            ob3 = data * ob1;
-            opt=1;
-            break;
-        case 4 :
-        {
-            if(ob1 == ob2)
-            {
-                cout<<"\nBoth Objects are equal or same value\n";
-            }
-            else
-            {
-                cout<<"\nThey are Unequal\n";
-            }
-            getch();
-
-            break;
-        }
-        case 5 :
-        {
-            if(ob1 < ob2)
-
-            {
-                cout<<"\nObject1 is Less than Object2\n";
-            }
-            else
-            {
-                cout<<"\nObject2 is Less than Object1\n";
-            }
-            getch();
-
-            break;
-        }
-        case 6 :
-        {
-            if(ob1 > ob2)
-            {
-                cout<<"\nObject1 is Greater than Object2\n";
-            }
-            else
-            {
-                cout<<"\nObject2 is Greater than Object1\n";
-            }
-            getch();
-
-            break;
-        }
-        case 0:
-            cout<<"\n\nHave a nice day....\n";
-            getch();
-            break;
-        default :
-            cout << "Invalid option ";
-        }
-        if (opt==1)
-        {
-            cout<<"\n\nResult in OBJECT3 as under\n";
+            cout << "\n\nResult in OBJECT3 as follows:\n";
             ob3.display();
             getch();
         }
     }
     return 1;
 }
-
-
--- polynimial.cpp
+++ polynimial.cpp
@@ -1,33 +1,68 @@
-/*
-1. Define an abstract data type - Poly, with three private data members a, b and c (type double) to
-represent the coefficients of a quadratic polynomial in the form:
-      ax^2 + bx + c
+#include <iostream>
+using namespace std;
 
-2. Include a parameter constructor in the Poly class to initialize all private data members with
-caller-supplied values.
+class Poly {
+private:
+    double a, b, c; // Coefficients of the polynomial
 
-3. Overload the addition operator to return the sum of two Poly objects.
+public:
+    // Parameterized constructor to initialize a, b, and c
+    Poly(double a = 0.0, double b = 0.0, double c = 0.0) : a(a), b(b), c(c) {}
 
-4. Overload the << (insertion) operator to output Poly objects in the following format,
-     e.g.,: ax^2 + bx + c
-where a, b and c are the data members of the Poly object. Do not display the a or b terms if they
-have zero coefficients. Moreover, if any coefficient is negative it should be precede by a minus sign,
-and not a plus sign.
+    // Overloaded addition operator to add two Poly objects
+    Poly operator+(const Poly& other) const {
+        return Poly(a + other.a, b + other.b, c + other.c);
+    }
 
-5. In your main() function, declare and initialize two Poly objects, q1 and q2, to represent the
-following polynomials: 3x^2 – 2 and -4x + 10 . Also declare a third, un-initialized Poly object
-named sum.
+    // Overloaded insertion operator to output Poly in the format "ax^2 + bx + c"
+    friend ostream& operator<<(ostream& out, const Poly& p) {
+        bool first = true;
+        
+        if (p.a != 0) {
+            out << p.a << "x^2";
+            first = false;
+        }
+        if (p.b != 0) {
+            if (!first) out << (p.b > 0 ? " + " : " - ");
+            else if (p.b < 0) out << "-";
+            out << (first || p.b < 0 ? abs(p.b) : p.b) << "x";
+            first = false;
+        }
+        if (p.c != 0) {
+            if (!first) out << (p.c > 0 ? " + " : " - ");
+            else if (p.c < 0) out << "-";
+            out << (first || p.c < 0 ? abs(p.c) : p.c);
+        }
+        if (p.a == 0 && p.b == 0 && p.c == 0)
+            out << "0";
+        return out;
+    }
 
-6. Output the sum of the two polynomials to the console using the following code exactly as it
-appears:
-sum = q1 + q2;
-cout << q1 << " : q1\n";
-cout << q2 << " : q2\n";
-cout << sum << " : q1+q2\n";
+    // Overloaded extraction operator to input Poly in the format "(a, b, c)"
+    friend istream& operator>>(istream& in, Poly& p) {
+        char ignore;
+        in >> ignore >> p.a >> ignore >> p.b >> ignore >> p.c >> ignore;
+        return in;
+    }
+};
 
-7. Overload the >> (extraction) operator to input a Poly object in the following format:
-     (a, b, c)
-where a, b and c are the data members of the poly object. The input will include the parentheses and
-commas as shown. Test the user input as "(-43, 60.32, -6.9)" and output this Poly object.
-*/
+int main() {
+    // Declare and initialize Poly objects
+    Poly q1(3, 0, -2);     // Represents 3x^2 - 2
+    Poly q2(0, -4, 10);    // Represents -4x + 10
+    Poly sum;
 
+    // Perform addition and output the results
+    sum = q1 + q2;
+    cout << q1 << " : q1\n";
+    cout << q2 << " : q2\n";
+    cout << sum << " : q1+q2\n";
+
+    // Test extraction operator
+    Poly userPoly;
+    cout << "\nEnter a polynomial in the format (a, b, c): ";
+    cin >> userPoly;
+    cout << "You entered: " << userPoly << endl;
+
+    return 0;
+}
